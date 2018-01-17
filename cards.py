599753f5a3b797ad846d004e7788a27df48384df
Anndo Ko <handcuffanndo@gmail.com>
2018-01-17 12:54:27 -0500

    plan the hand class

--- cards.py
+++ cards.py
@@ -215,6 +215,34 @@ class TestCard(unittest.TestCase):
 		pass
 
 # PART 2:
+class Hand:
+	# create the Hand with an initial set of cards
+	# param: a list of cards
+	def __init__(self, init_cards):
+		pass
+
+	# add a card to the hand
+	# silently fails if the card is already in the hand
+	# param: the card to add
+	# returns: nothing
+	def add_card(self, card):
+		pass
+
+	# remove a card from the hand
+	# param: the card to remove
+	# returns: the card, or None if the card was not in the Hand
+	def remove_card(self, card):
+		pass
+
+
+	# draw a card from a deck and add it to the hand
+	# side effect: the deck will be depleted by one card
+	# param: the deck from which to draw
+	# returns: nothing
+	def draw(self, deck):
+		pass
+
+
 class TestHand(unittest.TestCase):
 
 	# Test that a hand is initialized properly.
