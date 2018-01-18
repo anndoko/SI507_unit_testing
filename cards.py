53bbcda9f2625924e51084c49ae688f3c63f927b
Anndo Ko <handcuffanndo@gmail.com>
2018-01-17 20:14:46 -0500

    implement the Hand class

--- cards.py
+++ cards.py
@@ -219,29 +219,34 @@ class Hand:
 	# create the Hand with an initial set of cards
 	# param: a list of cards
 	def __init__(self, init_cards):
-		pass
+		self.cards = []
+		for card in init_cards:
+			self.cards.append(card)
 
 	# add a card to the hand
 	# silently fails if the card is already in the hand
 	# param: the card to add
 	# returns: nothing
 	def add_card(self, card):
-		pass
+		if card not in self.cards:
+			self.cards.append(card)
 
 	# remove a card from the hand
 	# param: the card to remove
 	# returns: the card, or None if the card was not in the Hand
 	def remove_card(self, card):
-		pass
-
-
+		if card in self.cards:
+			self.cards.remove(card)
+			return card
+		else:
+			return None
 	# draw a card from a deck and add it to the hand
 	# side effect: the deck will be depleted by one card
 	# param: the deck from which to draw
 	# returns: nothing
 	def draw(self, deck):
-		pass
-
+		card = deck.pop_card()
+		self.cards.append(card)
 
 class TestHand(unittest.TestCase):
 
